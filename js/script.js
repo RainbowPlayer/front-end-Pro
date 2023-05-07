16bedb3ee7501c176f2a06753a9df3c50407cfd3
RainbowPlayer <kolak375@gmail.com>
2023-05-07 23:19:41 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 const yearOfBirthday = +prompt('Please enter your year of birthday');
+let age;
 
 if(!yearOfBirthday) {
   alert('Okey, without your birthday');
-  const age = ('I dont know your age')
+  age = ('I dont know your age');
 } else{
-  const age = (`You are ${2023 - yearOfBirthday} years old`)
+  age = (`You are ${2023 - yearOfBirthday} years old`);
 }
 
 const city = prompt('Please enter your city');
-if (!city){
-  alert('Okey, without your city');
-}
+if (!city) alert('Okey, without your city');
 
 switch (city) {
 	case 'Kyiv':
@@ -22,15 +21,35 @@ switch (city) {
   case 'Washington':
   	alert (`You live in a capitale of USA. ${age}`);
     break;
-    case null:
-      alert (`I dont know your city ${age}`)
-      break;
+  case null:
+    alert (`I dont know your city. ${age}`);
+    break;
   default:
-    alert (`You live in ${city}. ${age}`)
+    alert (`You live in ${city}. ${age}`);
     break;
 }
 
 console.log (city)
 
-// const sport = prompt('Plaese enter your sport')
-// if (!sport) alert('Okey, without your sport')
+const sport = prompt('Plaese enter your sport');
+if (!sport) alert('Okey, without your sport');
+
+switch (sport){
+  case 'football':
+    alert (`Oh, greate choise. Do you like Messi?`);
+    break;
+  case 'tennis':
+    alert (`I like it too. Do you know smth about Federer`);
+    break;
+  case 'bascketball':
+    alert (`Nice choise. I know only Jordan, do you?`);
+    break;
+  case null:
+    alert (`I dont know about your favorite sport`);
+    break;
+  default:
+    alert (`Nice choise. Tell me more about ${sport}`);
+    break;
+}
+
+console.log (sport)
