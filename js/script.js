248a10ac04ac9862f255f420003a7576f6e26ce6
RainbowPlayer <kolak375@gmail.com>
2023-05-09 12:25:15 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,55 +1,24 @@
-const yearOfBirthday = +prompt('Please enter your year of birthday');
-let age;
+let operation = prompt(`Type operation like 'div', 'add', 'sub', 'mult'.`)
 
-if(!yearOfBirthday) {
-  alert('Okey, without your birthday');
-  age = ('I dont know your age');
-} else{
-  age = (`You are ${2023 - yearOfBirthday} years old`);
+const firsr = +prompt(`Type first number`);
+const second = +prompt(`Type second number`);
+let result
+
+if (operation === 'add') {
+  result = firsr + second;
+  alert (result);
+}else if (operation === 'mult') {
+  result = firsr * second;
+  alert (result);
+}else if (operation === 'sub') {
+  result = firsr - second;
+  alert (result);
+}else if (operation === 'div'){
+  result = firsr / second;
+  alert (result);
+}else{
+  alert ('Error')
 }
 
-const city = prompt('Please enter your city');
-if (!city) alert('Okey, without your city');
 
-switch (city) {
-	case 'Kyiv':
-  	alert (`You live in a capitale of Ukraine. ${age}19`);
-    break;
-  case 'London':
-  	alert (`You live in a capitale of Great Britain. ${age}`);
-    break;
-  case 'Washington':
-  	alert (`You live in a capitale of USA. ${age}`);
-    break;
-  case null:
-    alert (`I dont know your city. ${age}`);
-    break;
-  default:
-    alert (`You live in ${city}. ${age}`);
-    break;
-}
-
-console.log (city)
-
-const sport = prompt('Plaese enter your sport');
-if (!sport) alert('Okey, without your sport');
-
-switch (sport){
-  case 'football':
-    alert (`Oh, greate choise. Do you like Messi?`);
-    break;
-  case 'tennis':
-    alert (`I like it too. Do you know smth about Federer`);
-    break;
-  case 'bascketball':
-    alert (`Nice choise. I know only Jordan, do you?`);
-    break;
-  case null:
-    alert (`I dont know about your favorite sport`);
-    break;
-  default:
-    alert (`Nice choise. Tell me more about ${sport}`);
-    break;
-}
-
-console.log (sport)
+console.log(result)
