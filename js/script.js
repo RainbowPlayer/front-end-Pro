1ec0fd34921cf30444ed98fb8bbf2c035f34e565
RainbowPlayer <kolak375@gmail.com>
2023-05-11 18:52:38 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,24 +1,76 @@
-let operation = prompt(`Type operation like 'div', 'add', 'sub', 'mult'.`)
+let str = '';
 
-const firsr = +prompt(`Type first number`);
-const second = +prompt(`Type second number`);
-let result
-
-if (operation === 'add') {
-  result = firsr + second;
-  alert (result);
-}else if (operation === 'mult') {
-  result = firsr * second;
-  alert (result);
-}else if (operation === 'sub') {
-  result = firsr - second;
-  alert (result);
-}else if (operation === 'div'){
-  result = firsr / second;
-  alert (result);
-}else{
-  alert ('Error')
+for (let i = 20; i <= 30; i += 0.5) {
+  str += i + ' ';
+  
 }
 
+console.log(str);
 
-console.log(result)
+//1
+
+const oneDollar = 27;
+let convert = '';
+
+for (let o = 10; o <= 100; o += 10){
+  let grivnya = o * oneDollar;
+  convert += `${o} dollars ${grivnya} grivnays;\n `;
+}
+
+console.log(convert)
+
+//2
+
+let N = prompt(`Please tape number`);
+let twoLine = '';
+
+for (let j = 0; j <= 100; j++){
+ if (j * j <= N) {
+  twoLine += j + ` `;
+ }
+}
+
+console.log(twoLine);
+
+//3
+
+let m = prompt("Type a number");
+
+function isPrime(m) {
+    if (m <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(m); i++) {
+        if (m % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPrime(m));
+
+//4
+
+const f = +prompt(`type a number`);
+let found = false;
+
+for(let i = 1; i <= f; i++){
+   if (Math.pow(3, i) === f){
+    found = true;
+    break;
+   }
+}
+
+if (found){
+    alert(true);
+} else{
+    alert(false);
+}
+
+console.log(found);
+
+//5
+
