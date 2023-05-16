eb4b686332e1164c7afba3bcc4093ba61cd226d0
RainbowPlayer <kolak375@gmail.com>
2023-05-16 22:07:49 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 const massiveLong = +prompt(`How long massive we need?`);
 if (isNaN(massiveLong)){
     alert (`Not a number`);
-    break;
-}
+}else   {
 
-const array = [];
+let array = [];
 
 for(let i = 0; i < massiveLong; i++){
     array[i] = prompt(`enter item` + (i+1));
+    console.log(array);
 }
+
+alert(`Початковий масив ${array}`);
+console.log(`Початковий масив ${array}`);
 
 array.sort(function(a, b){
     return a - b;
 });
 
-alert(array)
+alert(`Відсортований масив ${array}`);
+console.log(`Відсортований масив ${array}`);
 
-array.splice(1, 3)
+array.splice(1, 3);
 
-alert(array)
+alert(`Видалив 2, 3, 4, елементи, лишилось ${array}`);
+console.log(`Видалив 2, 3, 4, елементи, лишилось ${array}`);
 
-console.log(array)
+};
+
